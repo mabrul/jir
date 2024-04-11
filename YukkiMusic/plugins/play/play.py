3d3739bc03365fb9948c89921c3e5d125bf4d647
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2024-04-11 20:33:17 +0700

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -30,6 +30,7 @@ from YukkiMusic.utils.database import is_video_allowed
 from YukkiMusic.utils.decorators.language import languageCB
 from YukkiMusic.utils.decorators.play import PlayWrapper
 from YukkiMusic.utils.formatters import formats
+from CilikMusic.utils.decorators.mwah import subcribe
 from YukkiMusic.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
